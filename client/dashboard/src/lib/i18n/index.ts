e4f79723d48b671507ad13a47e9bd9fa65e15d7b
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-07-24 23:30:59 +0300

    split the languages to separate files

--- client/dashboard/src/lib/i18n/index.ts
+++ client/dashboard/src/lib/i18n/index.ts
@@ -2,8 +2,34 @@ import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-import ar from "./ar.json";
-import en from "./en.json";
+import arAuth from "./ar/auth.json";
+import arBrands from "./ar/brands.json";
+import arCategories from "./ar/categories.json";
+import arCollections from "./ar/collections.json";
+import arCommon from "./ar/common.json";
+import arCustomers from "./ar/customers.json";
+import arDashboard from "./ar/dashboard.json";
+import arEmployees from "./ar/employees.json";
+import arOrders from "./ar/orders.json";
+import arProducts from "./ar/products.json";
+import arRoles from "./ar/roles.json";
+import arSeasons from "./ar/seasons.json";
+import arSettings from "./ar/settings.json";
+import arSidebar from "./ar/sidebar.json";
+import enAuth from "./en/auth.json";
+import enBrands from "./en/brands.json";
+import enCategories from "./en/categories.json";
+import enCollections from "./en/collections.json";
+import enCommon from "./en/common.json";
+import enCustomers from "./en/customers.json";
+import enDashboard from "./en/dashboard.json";
+import enEmployees from "./en/employees.json";
+import enOrders from "./en/orders.json";
+import enProducts from "./en/products.json";
+import enRoles from "./en/roles.json";
+import enSeasons from "./en/seasons.json";
+import enSettings from "./en/settings.json";
+import enSidebar from "./en/sidebar.json";
 
 const getDirection = (language: string) =>
   language === "ar"
@@ -25,8 +51,42 @@ if (!i18n.isInitialized) {
     .use(initReactI18next)
     .init({
       resources: {
-        en: { translation: en },
-        ar: { translation: ar },
+        en: {
+          translation: {
+            ...enCommon,
+            ...enSidebar,
+            ...enDashboard,
+            ...enCategories,
+            ...enBrands,
+            ...enCollections,
+            ...enSeasons,
+            ...enCustomers,
+            ...enProducts,
+            ...enOrders,
+            ...enRoles,
+            ...enEmployees,
+            ...enSettings,
+            ...enAuth,
+          },
+        },
+        ar: {
+          translation: {
+            ...arCommon,
+            ...arSidebar,
+            ...arDashboard,
+            ...arCategories,
+            ...arBrands,
+            ...arCollections,
+            ...arSeasons,
+            ...arCustomers,
+            ...arProducts,
+            ...arOrders,
+            ...arRoles,
+            ...arEmployees,
+            ...arSettings,
+            ...arAuth,
+          },
+        },
       },
       supportedLngs: ["en", "ar"],
       detection: {
